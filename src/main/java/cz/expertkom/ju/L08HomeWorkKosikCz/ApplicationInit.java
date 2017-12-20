b25ae31786eb3c76b8fde3e5cb4fa01b1f4488a0
roman <roman@LAPTOP-CKM3D2EK>
2017-12-20 20:06:52 +0100

    vytěžení Uri

--- src/main/java/cz/expertkom/ju/L08HomeWorkKosikCz/ApplicationInit.java
+++ src/main/java/cz/expertkom/ju/L08HomeWorkKosikCz/ApplicationInit.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cz.expertkom.ju.L08HomeWorkKosikCz.interfaces.DownLoadPageService;
+import cz.expertkom.ju.L08HomeWorkKosikCz.services.DownLoadPageServiceImpl;
 
 @Service
 public class ApplicationInit {
@@ -17,8 +18,7 @@ public class ApplicationInit {
 	public void initMethod(){
 		System.out.println("\nStart inicializace...");
 		dwnlps.start();
-
-		
+		System.out.println("\n...Konec inicializace");
 	}
 
 }
