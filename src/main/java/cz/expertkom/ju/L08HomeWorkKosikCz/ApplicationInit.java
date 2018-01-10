af5e6cb700cf6f9a0dca6bf178d0f999528c948b
roman <roman@LAPTOP-CKM3D2EK>
2018-01-10 20:48:07 +0100

    c

--- src/main/java/cz/expertkom/ju/L08HomeWorkKosikCz/ApplicationInit.java
+++ src/main/java/cz/expertkom/ju/L08HomeWorkKosikCz/ApplicationInit.java
@@ -1,24 +1,48 @@
 package cz.expertkom.ju.L08HomeWorkKosikCz;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cz.expertkom.ju.L08HomeWorkKosikCz.interfaces.DownLoadPageService;
-import cz.expertkom.ju.L08HomeWorkKosikCz.services.DownLoadPageServiceImpl;
+import cz.expertkom.ju.L08HomeWorkKosikCz.services.PullWebpageParseJsoup;
+import cz.expertkom.ju.L08HomeWorkKosikCz.services.TestJsoup;
 
 @Service
 public class ApplicationInit {
 	
 	//@Autowired
 	//DownLoadPageService dwnlps;
+	@Autowired
+	 PullWebpageParseJsoup pullWebpageParseJsoup;
+	
+	@Autowired
+	TestJsoup testJsoup;
 	
 	@PostConstruct
 	public void initMethod(){
 		//System.out.println("\nStart inicializace...");
 		//dwnlps.start();
 		//System.out.println("\n...Konec inicializace");
+	/*
+		try {
+			testJsoup.jsoupRHR();
+		} catch (IOException e) {
+	
+			e.printStackTrace();
+		}
+	*/
+		try {
+			pullWebpageParseJsoup.start();
+			//testJsoup.jsoupRHRproduct();
+			//testJsoup.jsoupUries();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
